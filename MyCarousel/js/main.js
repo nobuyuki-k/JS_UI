8f6d2d5ecb5afeaca5454f610ea9b4e1e09b6103
nobuyuki-k <nobuyuki.komai@gmail.com>
2022-03-24 06:29:42 +0900

    カルーセル18丸いボタンを動的に生成する

--- MyCarousel/js/main.js
+++ MyCarousel/js/main.js
@@ -5,6 +5,7 @@
   const prev = document.getElementById('prev');
   const ul = document.querySelector('ul');
   const slides = ul.children;
+  const dots = [];
   let currentIndex = 0;
 
   function updateButtons() {
@@ -14,18 +15,29 @@
     if (currentIndex === 0) {
       prev.classList.add('hidden');
     }
-
     if (currentIndex === slides.length - 1) {
       next.classList.add('hidden');
     }
   }
 
-  function moveSlides(){
+  function moveSlides() {
     const slideWidth = slides[0].getBoundingClientRect().width;
     ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
   }
 
+  function setupDots() {
+    for (let i = 0; i < slides.length; i++) {
+      const button = document.createElement('button');
+      dots.push(button);
+      document.querySelector('nav').appendChild(button);
+    }
+
+    dots[0].classList.add('current');
+  }
+
   updateButtons();
+  setupDots();
+
   next.addEventListener('click', () => {
     currentIndex++;
     updateButtons();
